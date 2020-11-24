468cc5081433913716e9db9af29a62d69b09c254
Pedro Grachet <pedro.grachet@gmail.com>
2020-11-24 17:09:34 -0400

    Criando UserMap e Configurando sua construção
    Criando UserMap

---
+++ CSharpBasico(Udemy)/Data/Mapping/UserMap.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data.Mapping
+{
+    public class UserMap : IEntityTypeConfiguration<UserEntity>
+    {
+        public void Configure(EntityTypeBuilder<UserEntity> builder)
+        {
+            builder.ToTable("User");
+            builder.HasKey(p => p.Id);
+            
+            builder.HasIndex(p => p.Email).IsUnique();
+
+            builder.Property(u => u.Name).IsRequired().HasMaxLength(60);
+
+            builder.Property(u => u.Email).HasMaxLength(100);
+        }
+    }
+}
