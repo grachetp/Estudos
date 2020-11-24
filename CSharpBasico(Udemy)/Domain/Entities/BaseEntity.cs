a8cd9c01190b6f33fbb11ed3c098b924fd248a1f
Pedro Grachet <pedro.grachet@gmail.com>
2020-11-24 16:20:28 -0400

    Implementação BaseEntity

---
+++ CSharpBasico(Udemy)/Domain/Entities/BaseEntity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Domain.Entities
+{
+    public abstract class BaseEntity
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        private DateTime? _createdAt;
+        public DateTime? CreatedAt
+        {
+            get { return _createdAt; }
+            set { _createdAt = (value == null ? DateTime.Now : value); }
+        }
+
+        public DateTime? UpdateAt { get; set; }
+
+    }
+}
