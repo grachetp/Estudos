283d886a739d8261a25f279acad1a154ccd579d3
Pedro Grachet <pedro.grachet@gmail.com>
2020-11-24 16:53:23 -0400

    Criando Context Factory

---
+++ CSharpBasico(Udemy)/Data/Context/ContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data.Context
+{
+    public class ContextFactory : IDesignTimeDbContextFactory<MyContext>
+    {
+        public MyContext CreateDbContext(string[] args)
+        {
+            //Usado para criar migrações
+            var connectionString = "Server=localhost;Port=3306;Database=dbAPI;Uid=root;Pwd=graxinha";
+            var optionsBuilder = new DbContextOptionsBuilder<MyContext>();
+
+            optionsBuilder.UseMySql(connectionString);
+
+            return new MyContext(optionsBuilder.Options);
+        }
+    }
+}
