8b6ac7529f290b27a2b20a35ceb45e2b05546faf
Pedro Grachet <pedro.grachet@gmail.com>
2020-11-24 17:46:01 -0400

    Criando Método InsertAsync

---
+++ CSharpBasico(Udemy)/Data/Repository/BaseRepository.cs
@@ -0,0 +1,67 @@
+﻿using Data.Context;
+using Domain.Entities;
+using Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Repository
+{
+    public class BaseRepository<T> : IRepository<T> where T : BaseEntity
+    {
+
+        protected readonly MyContext _context;
+        private DbSet<T> _dataset;
+
+        public BaseRepository(MyContext context)
+        {
+            _context = context;
+            _dataset = _context.Set<T>();
+        }
+
+        public Task<bool> DeleteAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<T> InsertAsync(T item)
+        {
+            try
+            {
+                if(item.Id == Guid.Empty)
+                {
+                    item.Id = Guid.NewGuid();
+                }
+
+                item.CreatedAt = DateTime.UtcNow;
+                
+                _dataset.Add(item);
+
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            return item;
+        }
+
+        public Task<T> SelectAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> SelectAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> UpdateAsync(T item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
