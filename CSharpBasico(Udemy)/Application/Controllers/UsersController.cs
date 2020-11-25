3d8cb42ba748dae1f7dc07081d8319338dabe550
Pedro Grachet <pedro.grachet@gmail.com>
2020-11-25 09:43:22 -0400

    Criando Método GetAll

--- CSharpBasico(Udemy)/Application/Controllers/UsersController.cs
+++ CSharpBasico(Udemy)/Application/Controllers/UsersController.cs
@@ -1,7 +1,32 @@
-﻿namespace Application.Controllers
+﻿using Domain.Interfaces.Services.User;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Application.Controllers
 {
-    public class UsersController
+    [Route("api/[users]")]
+    [ApiController]
+    public class UsersController : ControllerBase
     {
-        [Route]
+        [HttpGet]
+        public async Task<ActionResult> GetAll([FromServices] IUserService service)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState); //Retorna o código 400 - Solicitação Inválida
+            }
+
+            try
+            {
+                return Ok(await service.GetAll());
+            }
+            catch (ArgumentException e)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+
     }
 }
