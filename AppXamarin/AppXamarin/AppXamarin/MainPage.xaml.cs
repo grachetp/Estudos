81f2f4b5ea5366ebb23448dfe5e8cfd61361382c
Pedro Grachet <pedro.grachet@gmail.com>
2020-12-03 09:48:34 -0400

    Alterações e projeto incial com xamarin

---
+++ AppXamarin/AppXamarin/AppXamarin/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppXamarin
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        int count = 0;
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            count++;
+            if(count <= 1)
+            {
+                ((Button)sender).Text = $"Eu te amo {count} vez.";
+            }
+            else
+            {
+                ((Button)sender).Text = $"Eu te amo {count} vezes.";
+            }
+            
+        }
+    }
+}
