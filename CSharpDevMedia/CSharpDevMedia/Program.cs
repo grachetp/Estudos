a10bf6cc1443465ab9e3f2916c8bdb3312ce444e
Pedro Grachet <pedro.grachet@gmail.com>
2020-12-09 16:19:51 -0400

    novo projeto devmedia

---
+++ CSharpDevMedia/CSharpDevMedia/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpDevMedia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
