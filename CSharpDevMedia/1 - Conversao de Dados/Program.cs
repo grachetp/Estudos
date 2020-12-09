f5c5aaa40718f7d3f78bbedf9484ad9b5ec1d246
Pedro Grachet <pedro.grachet@gmail.com>
2020-12-09 17:10:00 -0400

    conversao de dados implicita

---
+++ CSharpDevMedia/1 - Conversao de Dados/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _1___Conversao_de_Dados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            sbyte a = 10;
+            short b = a;
+            int c = b;
+            long d = c;
+            float e = d;
+            double f = e;
+
+            Console.Write(f);
+            Console.Read();
+        }
+    }
+}
