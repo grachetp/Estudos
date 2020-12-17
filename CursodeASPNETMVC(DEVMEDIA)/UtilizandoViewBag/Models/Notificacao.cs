88663b585d1b63f8432f66ed0e8d648756ba11fb
Pedro Grachet <pedro.grachet@gmail.com>
2020-12-17 17:58:29 -0400

    Utilizando ViewBag para passar dados secundários

---
+++ CursodeASPNETMVC(DEVMEDIA)/UtilizandoViewBag/Models/Notificacao.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UtilizandoViewBag.Models
+{
+    public class Notificacao
+    {
+		public int Id { get; set; }
+		public string Titulo { get; set; }
+
+		public Notificacao(int id, string titulo)
+		{
+			Id = id;
+			Titulo = titulo;
+		}
+	}
+}
