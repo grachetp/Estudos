3fd4db0d75a3eeb5d6596d7170f3f65842c4f6d0
Pedro Grachet <pedro.grachet@gmail.com>
2020-12-17 15:01:59 -0400

    Criando Portal de Noticias e adicional model Noticia

---
+++ CursodeASPNETMVC(DEVMEDIA)/PortalDeNoticias/Models/Noticia.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PortalDeNoticias.Models
+{
+    public class Noticia
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Autor { get; set; }
+        public DateTime Date { get; set; }
+        public string Conteudo { get; set; }
+    }
+}
