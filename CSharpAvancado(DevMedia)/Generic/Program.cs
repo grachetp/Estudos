9d183aac42259c64998f9db3c1a4a62b19f6dfd2
Pedro Grachet <pedro.grachet@gmail.com>
2021-01-14 17:10:05 -0400

    Genérics e parametros nomeados

---
+++ CSharpAvancado(DevMedia)/Generic/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Generic
+{
+    public class Pilha<T>
+    {
+        int pos = 0;
+        T[] itens = new T[100];
+        public void Push(T item)
+        {
+            itens[pos] = item;
+            pos++;
+        }
+        public T Pop()
+        {
+            pos--;
+            return itens[pos];
+        }
+    }
+
+    public class Cliente
+    {
+        public int Codigo { get; set; }
+        public string Nome { get; set; }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p1 = new Pilha<string>();
+            var p2 = new Pilha<int>();
+            var p3 = new Pilha<Cliente>();
+        }
+    }
+}
