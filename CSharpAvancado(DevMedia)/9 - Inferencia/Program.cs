d4c9f72d909d434cf392d0c17ffc3b2e406e1bc6
Pedro Grachet <pedro.grachet@gmail.com>
2021-01-14 14:13:54 -0400

    Statics

---
+++ CSharpAvancado(DevMedia)/9 - Inferencia/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _9___Inferencia
+{
+    public class Connection
+    {
+        public string ConnectionString;
+        public void Conectar()
+        {
+            Console.WriteLine("Conectando...");
+        }
+        public void Fechar()
+        {
+            Console.WriteLine("Fechando Conexão...");
+        }
+    }
+    
+    public static class Fabrica
+    {
+        public static Connection CriarConnection()
+        {
+            return new Connection();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            dynamic con = Fabrica.CriarConnection();
+            con.Conectar();
+            con.Fechar();
+        }
+    }
+}
