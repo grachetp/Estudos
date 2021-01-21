eeefd7f73ab3d163683db282a87dc5fcd11332da
Pedro Grachet <pedro.grachet@gmail.com>
2021-01-20 22:25:28 -0400

    delegate

---
+++ CSharpAvancado(DevMedia)/Delegacao/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Delegacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pedido = new Pedido();
+            pedido.Fechar(2000);
+        }
+    }
+}
